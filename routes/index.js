34800f90f94839a5583a8fba86e17498beede015
Stanislav Bozhanov <stanislav_bozhanov@epam.com>
2018-05-11 10:08:56 +0300

    Add initial project structure

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    message: 'Welcome to KNAB SDK PoC'
+  }));
+});
+
+module.exports = router;
